5ac470945a61bac18e309e801014cb8f81c6bb7e
Kadphol <kadphol.len@outlook.co.th>
2022-03-07 00:24:45 +0700

    Uploader and UploadCard test update

--- frontend/src/components/uploadCard/uploadCard.test.tsx
+++ frontend/src/components/uploadCard/uploadCard.test.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { screen, render } from '@testing-library/react';
 import UploadCard from './uploadCard.component';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+
+let wrapper: { contains: (arg0: JSX.Element) => any; };
 
 describe('<UploadCard />', () => {
+  beforeEach(() => {
+    wrapper = mount(<UploadCard progress={10} fileName={"somefile.csv"}/>);
+  })
   it('should render uploader without error', () => {
-    const wrapper = mount(<UploadCard progress={10} fileName={"somefile.csv"}/>);
-  
+
     expect(wrapper.contains(<UploadCard progress={10} fileName={"somefile.csv"}/>)).toEqual(true);
   });
+
+  it('should render upload filename', () => {
+    render(<UploadCard progress={10} fileName={"somefile.csv"}/>);
+
+    expect(screen.getByText(/somefile.csv/)).toBeInTheDocument();
+  });
+
+  it('should render upload percentage', () => {
+    render(<UploadCard progress={10} fileName={"somefile.csv"}/>);
+
+    expect(screen.getByText(/10%/)).toBeInTheDocument();
+  })
 });
--- frontend/src/components/uploader/uploader.test.tsx
+++ frontend/src/components/uploader/uploader.test.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Uploader from './uploader.component';
 import { mount } from 'enzyme';
 
+let wrapper: { contains: (arg0: JSX.Element) => any; };
 describe('<Uploader />', () => {
+  beforeEach(() => {
+    wrapper = mount(<Uploader />);
+  });
+
   it('should render uploader without error', () => {
-    const wrapper = mount(<Uploader />);
-  
     expect(wrapper.contains(<Uploader />)).toEqual(true);
   });
 });
