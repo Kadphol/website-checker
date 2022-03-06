c133937d2c2d4a0c26d4effe37f0cbc7d75ab1bd
Kadphol <kadphol.len@outlook.co.th>
2022-03-06 19:55:48 +0700

    Add Testin backend

---
+++ backend/test/server.test.js
@@ -0,0 +1,62 @@
+const chai = require('chai'),
+  chaiHttp = require('chai-http'),
+  should = chai.should();
+
+const PORT = process.env.PORT || 8080;
+const APP_URL = 'http://localhost:' + PORT;
+chai.use(chaiHttp);
+
+describe('Servers test', function() {
+  before(function() {
+    require('../src/server')(process.cwd(), app => {
+      app.listen(PORT, err => {
+        console.log(err || 'Server running on ', PORT,
+          ' Process ID: ' + process.pid);
+      });
+    });
+  });
+
+  it('upload route success', function(done) {
+    chai.request(APP_URL)
+      .post('/upload')
+      .attach('file', __dirname + "/upload/site.csv")
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        done();
+      });
+  });
+
+  it('upload route wrong type of file', function(done) {
+    chai.request(APP_URL)
+      .post('/upload')
+      .attach('file', __dirname + "/upload/test.txt")
+      .end(function(err, res) {
+        res.should.have.status(500);
+        done();
+      });
+  });
+
+  it('upload route no file', function(done) {
+    chai.request(APP_URL)
+      .post('/upload')
+      .end(function(err, res) {
+        res.should.have.status(500);
+        done();
+      });
+  });
+
+  it('upload correct response', function(done) {
+    chai.request(APP_URL)
+      .post('/upload')
+      .attach('file', __dirname + "/upload/test.csv")
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.have.length(100);
+        done();
+      });
+  });
+});
